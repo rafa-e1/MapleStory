6299070335b3cdb758b9383dd555c8a212968787
RAFA <rafa71802937@gmail.com>
2024-11-14 00:53:31 +0900

    feat: 알림창 프로토콜 구현

---
+++ MapleStory/MapleStory/Sources/Utilities/Protocol/AlertDisplayable.swift
@@ -0,0 +1,40 @@
+//
+//  AlertDisplayable.swift
+//  MapleStory
+//
+//  Created by RAFA on 11/14/24.
+//
+
+import UIKit
+
+protocol AlertDisplayable: AnyObject {
+    func showAlert(
+        title: String,
+        message: String,
+        actionTitle: String,
+        completion: (() -> Void)?
+    )
+}
+
+extension AlertDisplayable where Self: UIViewController {
+
+    func showAlert(
+        title: String,
+        message: String,
+        actionTitle: String = "OK",
+        completion: (() -> Void)? = nil
+    ) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+
+        let action = UIAlertAction(title: actionTitle, style: .default) { _ in
+            completion?()
+        }
+
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
