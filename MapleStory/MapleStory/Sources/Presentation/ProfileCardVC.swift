4761799cba604728b7773ab42390c3f68422c7d9
RAFA <rafa71802937@gmail.com>
2024-11-14 18:26:42 +0900

    feat: 캐릭터 명함 뷰 컨트롤러 UI 구현

--- MapleStory/MapleStory/Sources/Presentation/ProfileCardVC.swift
+++ MapleStory/MapleStory/Sources/Presentation/ProfileCardVC.swift
@@ -8,4 +8,59 @@
 import UIKit
 
 final class ProfileCardVC: BaseViewController {
+
+    // MARK: - Properties
+
+    private let frontView = FrontView()
+    private let backView = BackView()
+
+    private var isFrontVisible = true
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTapGestures()
+    }
+
+    // MARK: - Actions
+
+    @objc private func flipCard() {
+        let fromView = isFrontVisible ? frontView : backView
+        let toView = isFrontVisible ? backView : frontView
+
+        UIView.transition(
+            from: fromView,
+            to: toView,
+            duration: 0.6,
+            options: [.transitionFlipFromRight, .showHideTransitionViews]
+        ) { _ in
+            self.isFrontVisible.toggle()
+        }
+    }
+
+    // MARK: - Helpers
+
+    private func setTapGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(flipCard))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    // MARK: - UI
+
+    override func setStyle() {
+        frontView.isHidden = false
+        backView.isHidden = true
+    }
+
+    override func setHierarchy() {
+        view.addSubviews(frontView, backView)
+    }
+
+    override func setLayout() {
+        [frontView, backView].forEach { view in
+            view.snp.makeConstraints { $0.edges.equalToSuperview() }
+        }
+    }
 }
