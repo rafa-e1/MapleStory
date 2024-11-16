0d766ccec734299f8844034b28e1ca5b193dfe6c
RAFA <rafa71802937@gmail.com>
2024-11-16 12:28:35 +0900

    feat: 캐릭터 응답 데이터 전송 객체 정의
    - Decodable 프로토콜을 채택하여 JSON 디코딩 지원
    - 서버 응답에서 캐릭터 고유 식별자를 나타내는 ocid 프로퍼티 정의

---
+++ MapleStory/MapleStory/Sources/DTO/Response/CharacterResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterResponseDTO.swift
+//  MapleStory
+//
+//  Created by RAFA on 11/16/24.
+//
+
+import Foundation
+
+struct CharacterResponseDTO: Decodable {
+    let ocid: String
+}
