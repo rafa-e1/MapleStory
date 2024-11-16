eba425234059551505a42acf471c0f4a9c82935a
RAFA <rafa71802937@gmail.com>
2024-11-16 12:27:27 +0900

    feat: 캐릭터 요청 데이터 전송 객체 정의
    - Codable 프로토콜을 채택하여 JSON 인코딩 및 디코딩 지원

---
+++ MapleStory/MapleStory/Sources/DTO/Request/CharacterRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterRequestDTO.swift
+//  MapleStory
+//
+//  Created by RAFA on 11/16/24.
+//
+
+import Foundation
+
+struct CharacterRequestDTO: Codable {
+
+    let characterName: String
+}
