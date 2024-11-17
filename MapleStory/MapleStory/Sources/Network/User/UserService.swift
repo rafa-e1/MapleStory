a43fdec802fc17317bc2522911971a22b67528c5
RAFA <rafa71802937@gmail.com>
2024-11-17 17:34:35 +0900

    feat: 캐릭터 OCID 조회 서비스 구현

---
+++ MapleStory/MapleStory/Sources/Network/User/UserService.swift
@@ -0,0 +1,32 @@
+//
+//  UserService.swift
+//  MapleStory
+//
+//  Created by RAFA on 11/16/24.
+//
+
+import Foundation
+
+import Alamofire
+
+struct UserService {
+
+    static func getCharacterOCID(
+        characterName: String,
+        completion: @escaping (Result<String, NetworkError>) -> Void
+    ) {
+        let target = UserTarget.getCharacterOCID(characterName: characterName)
+
+        NetworkService.shared.request(
+            target: target,
+            responseType: CharacterOCIDResponseDTO.self
+        ) { result in
+            switch result {
+            case .success(let characterResponse):
+                completion(.success(characterResponse.ocid))
+            case .failure:
+                completion(.failure(.invalidResponse))
+            }
+        }
+    }
+}
