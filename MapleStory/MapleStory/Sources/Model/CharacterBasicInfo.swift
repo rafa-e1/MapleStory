36803c7f51f1ef591af4a76b4596849ba97c5a4b
RAFA <rafa71802937@gmail.com>
2024-11-18 15:12:05 +0900

    feat: 캐릭터 기본 정보 모델 구현

---
+++ MapleStory/MapleStory/Sources/Model/CharacterBasicInfo.swift
@@ -0,0 +1,26 @@
+//
+//  CharacterBasicInfo.swift
+//  MapleStory
+//
+//  Created by RAFA on 11/17/24.
+//
+
+import Foundation
+
+struct CharacterBasicInfo {
+
+    let date: String?
+    let characterName: String
+    let worldName: String
+    let characterGender: String
+    let characterClass: String
+    let characterClassLevel: String
+    let characterLevel: Int
+    let characterExp: Int64
+    let characterExpRate: String
+    let characterGuildName: String?
+    let characterImage: String?
+    let characterDateCreate: String
+    let accessFlag: String
+    let liberationQuestClearFlag: String
+}
